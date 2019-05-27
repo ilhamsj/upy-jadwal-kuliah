66dd0aad5ec437d3ae63fa94fda3a5a3ee500844
ilhamsj <ilhamsaputrajati@gmail.com>
2019-05-27 19:50:39 +0700

    create lecture  model and migrations

---
+++ database/migrations/2019_05_27_124847_create_lectures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->string('lecure_id');
+            $table->string('lecture_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+}
