9805bccc7b20f07101b1f10c3611b2b6aaad33a5
ilhamsj <ilhamsaputrajati@gmail.com>
2019-05-27 17:55:29 +0700

    create mgrations and controller

---
+++ database/migrations/2019_05_27_104613_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->string('course_id', 10)->primary();
+            $table->string('course_name', 30);
+            $table->integer('sks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
