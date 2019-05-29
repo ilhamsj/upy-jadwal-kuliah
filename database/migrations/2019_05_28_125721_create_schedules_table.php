7d708952f5bbbd63d20ac90911dc1c2966eb5fab
ilhamsj <ilhamsaputrajati@gmail.com>
2019-05-30 00:07:44 +0700

    schedule and table

---
+++ database/migrations/2019_05_28_125721_create_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('lecture_id', '12')->index();
+            $table->string('course_id', '12')->index();
+            $table->string('class_room', '12')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
