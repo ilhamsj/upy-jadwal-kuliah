df92cd71da636f07f77c90d56b7535adf45c5134
ilhamsj <ilhamsaputrajati@gmail.com>
2019-06-06 09:05:54 +0700

    renov

--- database/factories/MataKuliahFactory.php
+++ database/factories/MataKuliahFactory.php
@@ -2,10 +2,10 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model;
+use App\MataKuliah;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(MataKuliah::class, function (Faker $faker) {
     return [
         //
     ];
