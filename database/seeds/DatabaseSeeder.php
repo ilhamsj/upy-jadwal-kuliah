64ed063715846c8b2a7e3981384fdd0271fbe14d
ilhamsj <ilhamsaputrajati@gmail.com>
2019-06-06 11:35:21 +0700

    user seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(MataKuliahTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class, 
+            MataKuliahTableSeeder::class
+        ]);
+        // $this->call(MataKuliahTableSeeder::class);
     }
 }
